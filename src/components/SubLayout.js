75369b6185301cf17a4e8d3e2108e1c2eaed4d3f
psymoney <sykim@amcryptoteam.com>
2022-11-23 14:04:28 +0900

    [fix] apply new Link object usage
    Next Link object creates <a> tag so that no longer need to use <a> tag inside of <Link>.

--- src/components/SubLayout.js
+++ src/components/SubLayout.js
@@ -3,7 +3,9 @@ import Link from 'next/link'
 export default function SubLayout({ children }) {
   return (
     <div>
-      <h1><Link href="/"><a>Home 으로</a></Link></h1>
+      <h1>
+        <Link href="/">Home 으로</Link>
+      </h1>
       {children}
     </div>
   )
